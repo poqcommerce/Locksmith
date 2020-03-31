df933c49a85aad826069f2e0bd917fe945b7d7f4
Joshua White <joshua.marc.white@outlook.com>
2020-03-31 14:02:57 +0100

    Fixed package to swift 5 and with path

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
+    products: [
+        .library(name: "Locksmith", targets: ["Locksmith"])
+    ],
+    targets: [
+        .target(name: "Locksmith", path: "Source")
+    ]
 )
